8208976a5e7ddd30ce09f3b2ec2277a16705f333
Anumart Chaichana <tonanumart@gmail.com>
2016-04-04 16:33:57 +0700

    Adding Check In And Check Out Parking Phase 1
    -Input and submit Data

--- CarPark/Controllers/HomeController.cs
+++ CarPark/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CarPark.Model;
+using CarPark.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -32,10 +34,39 @@ namespace CarPark.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult CheckOut(int Id)
+        {
+            using (var db = new CarParkDb())
+            {
+                Ticket ticket = db.Tickets.Find(Id);
+                ticket.DateOut = DateTime.Now;
+                db.SaveChanges();
+                TempData["LastTicket"] = ticket;
+            }
+            return RedirectToAction("CheckOut");
+        }
+
         public ActionResult CheckIn()
         {
             return View();
         }
-        
+
+        [HttpPost]
+        public ActionResult CheckIn(string PlateNo)
+        {
+            using (CarParkDb db = new CarParkDb())
+            {
+                Ticket ticket = new Ticket();
+                ticket.PlateNo = PlateNo;
+                ticket.DateIn = DateTime.Now;
+                db.Tickets.Add(ticket);
+                db.SaveChanges();
+                TempData["LastTicket"] = ticket;
+            }
+            return RedirectToAction("CheckIn");
+        }
+
+
     }
 }
